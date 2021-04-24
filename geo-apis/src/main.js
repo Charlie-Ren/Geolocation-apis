c0bedd5b4a3f8604d72d8678e235ab20fb0b2fb7
Charlie-Ren <540585507qq@gmail.com>
2021-04-24 01:06:52 -0500

    done

--- geo-apis/src/main.js
+++ geo-apis/src/main.js
@@ -8,7 +8,7 @@ new Vue({
   router,
   data: function(){
     return {
-        api_key: 'AIzaSyCHki8t8Ayr63B9HcVIG5g3cf7QnbrX2QM', //api key
+        api_key: process.env.api_key, //api key
     }
   },
   render: h => h(App)
