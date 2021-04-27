5ff3a46e31ee6f91546a861e3c3ceef85db67c8a
Charlie-Ren <540585507qq@gmail.com>
2021-04-27 00:10:47 -0500

    add done

--- geo-apis/src/views/geocode.vue
+++ geo-apis/src/views/geocode.vue
@@ -2,7 +2,7 @@
     <div class="geocode">
         This is geocode.
     <div>
-        <input v-model="location">
+        <input class="form-control" aria-label="Large" v-model="location" placeholder="Type your address" id="autocomplete">
     </div>
     <div>
         <button @click="geoCodeFunction">Submit</button>
@@ -13,6 +13,7 @@
     </li>
     </div>
 </template>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHki8t8Ayr63B9HcVIG5g3cf7QnbrX2QM"></script>
 <script>
 import axios from 'axios';
 export default {
@@ -25,9 +26,23 @@ export default {
             formatted_address: "",
         }
     },
+    //autocomplete part.
+    mounted()
+    {
+        new google.maps.places.Autocomplete(
+            document.getElementById("autocomplete"),
+            {
+                bounds: new google.maps.LatLngBounds(
+                    new google.maps.LatLng(42.0266573,-93.64645159999999)
+                )
+            }
+        )
+    },
     methods:{
         geoCodeFunction: function()
         { 
+            //how we use geocode api here.
+            //using axios to send    HTTP request
             axios.get("https://maps.googleapis.com/maps/api/geocode/json",{
                 params:{
                     address: this.location,
@@ -36,9 +51,17 @@ export default {
             })
             .then((response)=>{
                 console.log(response.data);
-                this.formatted_address=response.data.results[0].formatted_address;//get its formatted address
-                this.result=response.data.results[0].geometry.location;//get its location info
-                //U can also try to get other infos.
+                if(response.data.status=="OK")
+                {
+                    this.formatted_address=response.data.results[0].formatted_address;//get its formatted address
+                    this.result=response.data.results[0].geometry.location;//get its location info
+                    //U can also try to grab other infos.
+                }
+                else
+                {
+                    this.formatted_address="No matching info";
+                    this.result={lat:"null", lng:"null"};
+                }
             })
             .catch((error)=>{
                 console.log(error);
